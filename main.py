b363ff674f342d321abe5abf21b950cba9223485
Dishant Naik <dishantnaik1996@gmail.com>
2020-03-19 20:31:37 -0500

    Update main.py
    This is the final version of the python code which is developed for the purpose of technical assessment.
    This program checks weather string s1 and s2 can be mapped on one to one relationship or not
    Returns "true" if string s1 can be mapped to string s2 and returns "false" if string s1 can not be mapped to string s2  
    
    Note -
    I have not added a main block or print statements in the code, So to test the code add following code at the bottom of the program
    if __name__=='__main__':
        s1 = 'aab' 
        s2 = 'xxy'
        print (run(s1,s2))

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ Created on Wed Mar 18 17:16:43 2020
 @author: iCule10 (Dishant Naik)
 """
 def run(s1,s2):
+    #Both string must be of same length
     if(len(s1) == len(s2)):
         tmp_s1, tmp_s2 = {},{}
         
+        #Assigning key to each value in s1
         for i, val in enumerate(s1): tmp_s1[val] = tmp_s1.get(val,[]) + [i]
         
+        #Assigning key to each value in s2
         for i, val in enumerate(s2): tmp_s2[val] = tmp_s2.get(val,[]) + [i]
         
+        #Compare if number of distinc values + same same values(all same valses counts as 1 e.g (aab = [[0,1],2]))in s1 is equal to s2 then return true
+        #else return false
         if sorted(tmp_s1.values()) == sorted(tmp_s2.values()): return("true")
         else: return("false")
     else: return("false")
-    
-if __name__=='__main__':
-    s1 = 'ab' 
-    s2 = 'xxy'
-    print (run(s1,s2))
